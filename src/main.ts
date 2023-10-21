f04508e6f9a7280241965bd57243c73e7854f78d
Onwuzor Victor <onwuzorvictor@outlook.com>
2023-10-21 13:41:05 +0200

    add global prefix and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+
+  // endpoint prefix
+  app.setGlobalPrefix('api/v1');
+  // handle all user input validation globally
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(configService.get('PORT') || 4000);
 }
 bootstrap();
